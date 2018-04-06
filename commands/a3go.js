3146f7f1d80d2520cc5b6b7dce795f323fdcbd4d
SADPSRP <37345876+SADPSRP@users.noreply.github.com>
2018-04-06 09:10:49 -0400

    Add files via upload

---
+++ commands/a3go.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let sicon = message.guild.displayAvatarURL;
+  let botembed = new Discord.RichEmbed()
+  .setTitle("Grand Opening of SADPS Arma 3 Servers")
+  .setColor("#f4b042")
+  .addField("**I am happy to announce the opening of a Arma 3 Server which will be a milsim/war type of server, In the future we plan to launch a Life server on Arma 3**", "\u200b", true);
+
+  message.channel.send(botembed);
+
+}
+
+module.exports.help = {
+  name: "a3go"
+}
