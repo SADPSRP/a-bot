27ee392deff124257cd93a9a247854cd57a3dc43
SADPSRP <37345876+SADPSRP@users.noreply.github.com>
2018-04-06 09:19:14 -0400

    Update a3go.js

--- commands/a3go.js
+++ commands/a3go.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  let sicon = message.guild.displayAvatarURL;
+   let sicon = message.guild.displayAvatarURL;
   let botembed = new Discord.RichEmbed()
-  .setTimestamp()
   .setTitle("Grand Opening of SADPS Arma 3 Servers")
   .setColor("#f4b042")
-  .addField("**I am happy to announce the opening of a Arma 3 Server which will be a milsim/war type of server, In the future we plan to launch a Life server on Arma 3**", "\u200b", true);
-  .setFooter("-SADPS Administration & Development Team,", "\u200b")'
+  .addField("**I am happy to announce the opening of a Arma 3 Server which will be a milsim/war type of server, In the future we plan to launch a Life server on Arma 3**", "\u200b", true)
+  .setFooter("-SADPS Administration & Development Team,", "\u200b")
+  .setTimestamp();
   
   message.channel.send(botembed);
 
